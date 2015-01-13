dd9f50e0c4c2c1c94cb86b25fc8f8debb8a951da
Benny Siegert <bsiegert@gmail.com>
2015-01-13 21:36:58 +0100

    Show branch name on the Build Details page.
    Update issue #2.

--- templates.go
+++ templates.go
@@ -130,6 +130,8 @@ var TablePkgs = template.Must(template.New("TablePkgs").Parse(tablePkgs))
 const bulkBuildInfo = `
       <div class="col-md-8">
         <dl class="dl-horizontal" style="font-size: 120%">
+	  <dt>Branch</dt>
+	  <dd>{{.Branch}}</dd>
 	  <dt>Platform</dt>
 	  <dd>{{.Platform}}</dd>
 	  <dt>Compiler</dt>
