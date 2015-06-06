c1cc6fffedf9f63d12eafc1e1c5b355d330545bf
Benny Siegert <bsiegert@gmail.com>
2015-06-06 14:48:38 +0200

    Cache data for latestBuilds in memcache.

--- data/data.go
+++ data/data.go
@@ -5,12 +5,26 @@ package data
 import (
 	"github.com/bsiegert/BulkTracker/bulk"
 
+	"time"
+
 	"appengine"
 	"appengine/datastore"
+	"appengine/memcache"
 )
+
+const latestBuildsKey = "latestBuilds"
 
 // LatestBuilds fetches the list of latest builds to show on the landing page.
 func LatestBuilds(c appengine.Context) (builds []bulk.Build, err error) {
+	_, err = memcache.Gob.Get(c, latestBuildsKey, &builds)
+	if err != nil && err != memcache.ErrCacheMiss {
+		c.Warningf("get latest builds from memcache: %s", err)
+	}
+	if err == nil {
+		c.Debugf("latestBuilds: used cached result")
+		return builds, nil
+	}
+
 	keys, err := datastore.NewQuery("build").Order("-Timestamp").Limit(10).GetAll(c, &builds)
 	if err != nil {
 		return nil, err
@@ -18,5 +32,15 @@ func LatestBuilds(c appengine.Context) (builds []bulk.Build, err error) {
 	for i := range builds {
 		builds[i].Key = keys[i].Encode()
 	}
+
+	err = memcache.Gob.Set(c, &memcache.Item{
+		Key:        latestBuildsKey,
+		Object:     &builds,
+		Expiration: 30 * time.Minute,
+	})
+	if err != nil {
+		c.Warningf("failed to Set latestBuilds: %s", err)
+	}
+
 	return builds, nil
 }
