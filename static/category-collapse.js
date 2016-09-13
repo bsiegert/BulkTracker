95de6abc169bb85c34c1a5acdacf0c101402c8c3
Benny Siegert <bsiegert@gmail.com>
2016-09-13 18:46:37 +0200

    JS improvements for category-collapse.
    - namespacing (bt.categories)
    - fixed some accidental leakage of vars into global scope.
    - moved more code into the .js file

--- static/category-collapse.js
+++ static/category-collapse.js
@@ -1,34 +1,58 @@
-// AddCollapseHandler adds an on-click handler to the collapse link for
+// BulkTracker module.
+var bt = bt || {};
+bt.categories = bt.categories || {};
+
+// init fires off an AJAX request to populate the categories table.
+bt.categories.init = function () {
+  $.ajax({url: '/json/dir/'}).done(function (data) {
+    for (var i in data) {
+      if (data.hasOwnProperty(i)) {
+	bt.categories.addPanel(data[i]);
+	bt.categories.addHandler(data[i]);
+      }
+    }
+  });
+}
+
+// addHandler adds an on-click handler to the collapse link for
 // name. The handler loads a list of packages in that category via AJAX
 // and builds up a list.
-function AddCollapseHandler(name) {
+bt.categories.addHandler = function (name) {
   name = name.split("/")[0];
   $('a#'+name+'-collapse').click(function() {
     $.ajax({
       url: '/json/dir/'+name+'/',
-    }).done(function(data) {
-      _a = '<ul class="list-inline">';
+    }).done(function (data) {
+      var a = ['<ul class="list-inline">'];
       for (var i in data) {
-	_a += '<li class="column-item"><a href="/pkgresults/'
-	  + name + '/' + data[i] + '">' + data[i] + '</a></li>';
+	a.push('<li class="column-item"><a href="/pkgresults/');
+	a.push(name, '/', data[i], '">', data[i], '</a></li>');
       }
-      _a += '</ul>';
-      $('#'+name+'-body').html(_a);
+      a.push('</ul>');
+      $('#'+name+'-body').html(a.join(""));
     })
-    .fail(function() {
+    .fail(function () {
       $('#'+name+'-body').html('<p class="text-danger">Failed to load navigation.</p>');
     });
   });
 }
 
-function AddCollapsePanel(name) {
+bt.categories.addPanel = function (name) {
   name = name.split("/")[0];
-  _a = '<li class="column-item">'
-    + '<a id="' + name + '-collapse" data-toggle="collapse" href="#'
-    + name + '">' + name + '/</a></li>'
-    + '<div id="' + name + '" class="panel panel-default panel-collapse collapse">'
-    + '<div class="panel-heading">' + name + '</div>'
-    + '<div id="' + name + '-body" class="panel-body">'
-    + '<p class="text-muted">Loading ...</p></div></div>';
-  $('#categories').append(_a);
+  var a = [
+	  '<li class="column-item">',
+          '<a id="', name,
+	  '-collapse" data-toggle="collapse" href="#', name,
+	  '">',
+	  name,
+	  '/</a></li>',
+          '<div id="', name,
+	  '" class="panel panel-default panel-collapse collapse">',
+          '<div class="panel-heading">',
+	  name,
+	  '</div>',
+          '<div id="', name, '-body" class="panel-body">',
+          '<p class="text-muted">Loading ...</p></div></div>',
+  ];
+  $('#categories').append(a.join(""));
 }
