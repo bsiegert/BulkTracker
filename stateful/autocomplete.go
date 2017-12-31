1ded1a56509eb40342794dee99107d95b7276b5d
Benny Siegert <bsiegert@gmail.com>
2017-12-31 17:52:03 +0100

    Basic shape of autocomplete backend.
    Update #14.
    
    This contains a stateful handler (it keeps the list of packages
    in memory across requests) for autocomplete.

--- stateful/autocomplete.go
+++ stateful/autocomplete.go
@@ -5,27 +5,49 @@ package stateful
 import (
 	"context"
 	"fmt"
-	"net/url"
+	"strings"
 
 	"github.com/bsiegert/BulkTracker/bulk"
 	"google.golang.org/appengine/datastore"
 )
 
-var allPkgNames []string
+var ch chan AutocompleteRequest
+
+// TODO regularly reload the dataset.
 
 func load(ctx context.Context) error {
 	var pkgs []bulk.Pkg
-	_, err := datastore.NewQuery("pkg").Project("Category", "Dir").Distinct().GetAll(context.Background(), &pkgs)
+	_, err := datastore.NewQuery("pkg").Project("Category", "Dir").Distinct().GetAll(ctx, &pkgs)
 	if err != nil {
 		return err
 	}
 
-	result := make([]string, 0, len(pkgs))
+	allPkgNames := make([]string, 0, len(pkgs))
 	for _, p := range pkgs {
-		result = append(result, fmt.Sprintf("%s/%s", p.Category, p.Dir))
+		allPkgNames = append(allPkgNames, fmt.Sprintf("%s/%s", p.Category, p.Dir))
 	}
-	allPkgNames = result
+
+	newChan := make(chan AutocompleteRequest)
+	go func() {
+		for req := range newChan {
+			lookup(allPkgNames, req)
+		}
+	}()
+	ch = newChan
 	return nil
+}
+
+func lookup(allPkgNames []string, req AutocompleteRequest) {
+	resp := AutocompleteResponse{}
+	for _, p := range allPkgNames {
+		if strings.Contains(p, req.Search) {
+			resp.Results = append(resp.Results, Result{
+				ID:   p,
+				Text: p,
+			})
+		}
+	}
+	req.Ret <- resp
 }
 
 // Result encodes a single line of the select2 JSON response format.
@@ -41,7 +63,26 @@ type AutocompleteResponse struct {
 	}
 }
 
-func Autocomplete(ctx context.Context, _ []string, form url.Values) (interface{}, error) {
-	// TODO
-	return nil, nil
+// AutocompleteRequest has the parameters for a single autocomplete request.
+type AutocompleteRequest struct {
+	// ctx is the request context. It must be an App Engine context. It is
+	// only used for this one request.
+	Ctx context.Context
+	// search is the search string.
+	Search string
+	// ret is the channel in which to return the reponse.
+	Ret chan<- AutocompleteResponse
+}
+
+// Autocomplete sends an autocomplete request. The response will be delivered
+// over the channel in req.Ret. The function returns an error that occurs while
+// loading the dataset.
+func Autocomplete(req AutocompleteRequest) error {
+	if ch == nil {
+		if err := load(req.Ctx); err != nil {
+			return err
+		}
+	}
+	ch <- req
+	return nil
 }
