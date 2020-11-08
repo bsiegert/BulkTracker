991436a610fbb04e6e7718a7338fcc1dc2dfde03
Benny Siegert <bsiegert@gmail.com>
2020-11-08 12:20:26 +0100

    expireold: check whether we have details for a build.

--- expireold/go.mod
+++ expireold/go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	cloud.google.com/go/datastore v1.3.0
 	github.com/bsiegert/BulkTracker v0.0.0-20201024194704-90c091dc2286
+	google.golang.org/api v0.32.0
 )
 
 replace github.com/bsiegert/BulkTracker => ../
--- expireold/main.go
+++ expireold/main.go
@@ -8,6 +8,7 @@ import (
 
 	"cloud.google.com/go/datastore"
 	"github.com/bsiegert/BulkTracker/bulk"
+	"google.golang.org/api/iterator"
 )
 
 var numResults = flag.Int("n", 5, "Number of results")
@@ -21,19 +22,37 @@ func main() {
 		log.Fatal(err)
 	}
 	// Oldest builds.
-	q := datastore.NewQuery("build").Order("Timestamp").Limit(100)
+	q := OldestBuilds()
 	iter := client.Run(ctx, q)
 
 	var build bulk.Build
 
 	for i := 0; i < *numResults; {
 		key, err := iter.Next(&build)
-		if err != nil {
+		if err == iterator.Done {
+			break
+		} else if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Printf("%s (%v on %v)\n", key.Encode(), build.Platform, build.Date())
-		i++
+		if HasDetails(ctx, client, key) {
+			fmt.Println("\thas detailed records")
+			i++
+		}
 	}
 
 	client.Close()
+}
+
+func OldestBuilds() *datastore.Query {
+	return datastore.NewQuery("build").Order("Timestamp").Limit(100)
+}
+
+func HasDetails(ctx context.Context, client *datastore.Client, buildKey *datastore.Key) bool {
+	q := datastore.NewQuery("pkg").Ancestor(buildKey).KeysOnly().Limit(1)
+	keys, err := client.GetAll(ctx, q, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return len(keys) > 0
 }
