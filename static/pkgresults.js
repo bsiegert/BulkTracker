500da2e0527d3dff3a103a575f10bd0c240f6e58
Benny Siegert <bsiegert@gmail.com>
2021-01-20 21:08:48 +0100

    Add an error message div to package results.
    This displays an error if it was impossible to decode the package name,
    instead of defaulting to www/firefox.

--- static/pkgresults.js
+++ static/pkgresults.js
@@ -14,18 +14,23 @@ var classes = {
 };
 
 function PkgResultsTable(event) {
-  var pkgname = "www/firefox";
-  var pkgname_re = /^[A-Za-z0-9+-]+\/[A-Za-z0-9+-]+$/;
+  var pkgname = null;
+  var pkgname_re = /^[A-Za-z0-9+-_]+\/[A-Za-z0-9+-_]+$/;
   var fragment = window.location.pathname.split("/pkgresults/")[1];
   if (fragment && pkgname_re.test(fragment)) {
     pkgname = fragment;
+  } else {
+    let err = $('#error');
+    err.html("Failed to decode the package name from the URL.");
+    err.show();
+    $('#results').hide();
   }
   $('#pkgname-header').text(pkgname);
 
   $('.table').dataTable({
     destroy: true,
     ajax: {
-      url:     "/json/"+event.data+"/"+pkgname,
+      url:     `/json/${event.data}/${pkgname}`,
       dataSrc: ""
     },
     columns: [
