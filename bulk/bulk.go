2b7aad2530ef0c7f8f6092756252006dd49d206d
Benny Siegert <bsiegert@gmail.com>
2024-09-20 09:02:02 +0000

    Refactoring: move dependency fixup logic to its own function
    This can now be tested separately.

--- bulk/bulk.go
+++ bulk/bulk.go
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2014-2018, 2022-2023
+ * Copyright (c) 2014-2018, 2022-2024
  *      Benny Siegert <bsiegert@gmail.com>
  *
  * Provided that these terms and disclaimer and all copyright notices
@@ -126,8 +126,6 @@ func BuildFromReport(from string, r io.Reader) (*ddao.Build, error) {
 
 func PkgsFromReport(r io.Reader) ([]ddao.PkgResult, error) {
 	var pkgs []ddao.PkgResult
-	// Failed packages. The key is the name, the value an index into pkgs.
-	var failedPkgs = make(map[string]int)
 	var p *ddao.PkgResult
 	n := 0
 
@@ -150,15 +148,25 @@ func PkgsFromReport(r io.Reader) ([]ddao.PkgResult, error) {
 			p.Category, p.Dir = path.Split(string(val))
 		case bytes.Equal(key, []byte("BUILD_STATUS")):
 			p.BuildStatus = statuses[string(val)]
-			if p.BuildStatus != OK {
-				failedPkgs[p.PkgName] = n - 1
-			}
 		case bytes.Equal(key, []byte("DEPENDS")):
 			p.FailedDeps = string(val)
 		}
 	}
-	// Do another run over all indirect-failed packages, only keep
-	// dependencies that actually failed.
+
+	fixUpDependencies(pkgs)
+
+	return pkgs, s.Err()
+}
+
+// fixUpDependencies does another run over all indirect-failed packages and only keeps
+// dependencies that actually failed.
+func fixUpDependencies(pkg []ddao.PkgResult) {
+	// indices maps package name to its index in pkgs.
+	indices := make(map[string]int)
+	for i := range pkgs {
+		indices[pkgs[i].PkgName] = i
+	}
+
 	for i := range pkgs {
 		if pkgs[i].BuildStatus != IndirectFailed && pkgs[i].BuildStatus != IndirectPrefailed {
 			pkgs[i].FailedDeps = ""
@@ -166,11 +174,12 @@ func PkgsFromReport(r io.Reader) ([]ddao.PkgResult, error) {
 		failedDeps := strings.Fields(pkgs[i].FailedDeps)
 		f := make([]string, 0, len(failedDeps))
 		for _, dep := range failedDeps {
-			if fp, ok := failedPkgs[dep]; ok {
+			d := indices[dep]
+			if pkgs[d].BuildStatus != OK {
 				f = append(f, dep)
 				// TODO: if pkgs[fp] is indirect-failed, add to the counter of
 				// _its_ failed dependencies.
-				pkgs[fp].Breaks++
+				pkgs[d].Breaks++
 			}
 		}
 		if len(f) == 0 {
@@ -178,5 +187,4 @@ func PkgsFromReport(r io.Reader) ([]ddao.PkgResult, error) {
 		}
 		pkgs[i].FailedDeps = strings.Join(f, " ")
 	}
-	return pkgs, s.Err()
 }
