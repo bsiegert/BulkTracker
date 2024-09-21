0cb174514032f1ece1dedd08f5e830855620d29b
Benny Siegert <bsiegert@gmail.com>
2024-09-21 10:25:05 +0000

    Cleanup: fix build and update modules
    This fixes a few typos introduced in the last set of commits.
    It also contains the result of "go mod tidy". The set of dependencies
    is a bit smaller now.

--- bulk/bulk.go
+++ bulk/bulk.go
@@ -160,7 +160,7 @@ func PkgsFromReport(r io.Reader) ([]ddao.PkgResult, error) {
 
 // fixUpDependencies does another run over all indirect-failed packages and only keeps
 // dependencies that actually failed.
-func fixUpDependencies(pkg []ddao.PkgResult) {
+func fixUpDependencies(pkgs []ddao.PkgResult) {
 	// indices maps package name to its index in pkgs.
 	indices := make(map[string]int)
 	for i := range pkgs {
@@ -170,12 +170,13 @@ func fixUpDependencies(pkg []ddao.PkgResult) {
 	for i := range pkgs {
 		if pkgs[i].BuildStatus != IndirectFailed && pkgs[i].BuildStatus != IndirectPrefailed {
 			pkgs[i].FailedDeps = ""
+			continue
+
 		}
 		failedDeps := strings.Fields(pkgs[i].FailedDeps)
 		f := make([]string, 0, len(failedDeps))
 		for _, dep := range failedDeps {
-			d := indices[dep]
-			if pkgs[d].BuildStatus != OK {
+			if d, ok := indices[dep]; ok && pkgs[d].BuildStatus != OK {
 				f = append(f, dep)
 				// TODO: if pkgs[fp] is indirect-failed, add to the counter of
 				// _its_ failed dependencies.
